52463ba8f14ba7b97ca02d0f67b40a7330249bd2
Joel GM <34335312+JoelGMSec@users.noreply.github.com>
2023-04-20 11:31:25 +0200

    Update Invoke-Stealth

--- Invoke-Stealth.ps1
+++ Invoke-Stealth.ps1
@@ -5,10 +5,10 @@
 
 # Variables
 $ErrorActionPreference = "SilentlyContinue"
-$InvokePath=$($args[0])
-$Paramt=$($args[1])
-$Technique=$($args[2])
-$NoBanner=$($args[3])
+$InvokePath = $args[0]
+$Paramt = $args[1]
+$Technique = $args[2]
+$NoBanner = $args[3]
 
 # Banner
 function Show-Banner {
@@ -19,57 +19,55 @@ Write-host "  | || '_ \ \ / / _ \| |/ / _ \____\___ \| __/ _ \/ _' | | __| '_ \ 
 Write-host "  | || | | \ V / (_) |   <  __/_____|__) | ||  __/ (_| | | |_| | | | " -ForegroundColor Blue
 Write-host " |___|_| |_|\_/ \___/|_|\_\___|    |____/ \__\___|\__,_|_|\__|_| |_| " -ForegroundColor Blue
 Write-host
-Write-host "  ------------------------- by @JoelGMSec -------------------------  " -ForegroundColor Green }
+Write-host "  ------------------------- by @JoelGMSec -------------------------  " -ForegroundColor Green
+Write-host }
 
 # Help
 function Show-Help {
 Write-host ; Write-Host " Info: " -ForegroundColor Yellow -NoNewLine ; Write-Host " This tool helps you to automate the obfuscation process of"
 Write-Host "        any script written in PowerShell with different techniques"
-Write-Host ; Write-Host " Usage: " -ForegroundColor Yellow -NoNewLine ; Write-Host ".\Invoke-Stealth.ps1 script.ps1 -technique Chimera" -ForegroundColor Blue 
+Write-Host ; Write-Host " Usage: " -ForegroundColor Yellow -NoNewLine ; Write-Host ".\Invoke-Stealth.ps1 script.ps1 -technique Chameleon" -ForegroundColor Blue 
 Write-Host "         - You can use as single or separated by commas -" ; Write-Host ; Write-Host " Techniques: " -ForegroundColor Yellow 
-Write-Host "       · " -NoNewLine ; Write-Host "Chimera: "-ForegroundColor Green -NoNewLine ; Write-Host "Substitute strings and concatenate variables"
+Write-Host "       · " -NoNewLine ; Write-Host "Chameleon: "-ForegroundColor Green -NoNewLine ; Write-Host "Substitute strings and concatenate variables"
 Write-Host "       · " -NoNewLine ; Write-Host "BetterXencrypt: "-ForegroundColor Green -NoNewLine ; Write-Host "Compresses and encrypts with random iterations"
 Write-Host "       · " -NoNewLine ; Write-Host "PyFuscation: "-ForegroundColor Green -NoNewLine ; Write-Host "Obfuscate functions, variables and parameters"
-Write-Host "       · " -NoNewLine ; Write-Host "PSObfuscation: "-ForegroundColor Green -NoNewLine ; Write-Host "Convert content to bytes and compress with Gzip"
 Write-Host "       · " -NoNewLine ; Write-Host "ReverseB64: "-ForegroundColor Green -NoNewLine ; Write-Host "Encode with base64 and reverse it to avoid detections"
+Write-Host "       · " -NoNewLine ; Write-Host "PSObfuscation: "-ForegroundColor Green -NoNewLine ; Write-Host "Convert content to bytes and compress with Gzip"
 Write-Host "       · " -NoNewLine ; Write-Host "All: "-ForegroundColor Green -NoNewLine ; Write-Host "Sequentially executes all techniques described above"
 Write-Host ; Write-Host " Warning: " -ForegroundColor Red -NoNewLine  ; Write-Host "The output script will exponentially multiply the original size"
-Write-Host "         " -NoNewLine ; Write-Host " Chimera & PyFuscation need dependencies to work properly in Windows" ; Write-Host }
+Write-Host "         " -NoNewLine ; Write-Host " Chameleon & PyFuscation need dependencies to work properly in Windows" ; Write-Host }
 
-if(!$ScriptPath) { $InvokePath = "$pwd/$($args[0])" }
 if($NoBanner -notlike '*-nobanner') { Show-Banner } ; if($InvokePath -like '*-h*') { Show-Help ; break }
-f($Paramt -notlike '-t*') { Show-Help ; Write-Host "[!] Error: Not enough parameters!" -ForegroundColor Red ; Write-Host ; break }
 if(!$Technique) { Show-Help ; Write-Host "[!] Error: Not enough parameters!" -ForegroundColor Red ; Write-Host ; break }
 
-# Main
-Write-host ; $RandomNumber = Get-Random (10..25)
-$checkpath = Get-ChildItem $pwd\Resources ; if ($? -eq $true){ $local = "True" ; Get-ChildItem -Path $pwd -Recurse | Unblock-File }
+# Checks
+$CheckPath = Get-ChildItem $pwd\Resources ; $RandomNumber = Get-Random (10..25)
+if ($CheckPath){ $local = "True" ; Get-ChildItem -Path $pwd -Recurse | Unblock-File }
+if (($InvokePath -notlike "*/*") -and ($InvokePath -notlike "*\*")){ $InvokePath = "`"$pwd/$($args[0])`"" }
 
-function Test-Command {
-Param ($command)
-$oldPreference = $ErrorActionPreference
-$ErrorActionPreference = 'stop'
-try {if(Get-Command $command){RETURN $true}}
-Catch {RETURN $false}
-Finally {$ErrorActionPreference=$oldPreference}}
+# Functions
+function Test-Command { Param ($command)
+$oldPreference = $ErrorActionPreference ; $ErrorActionPreference = 'stop'
+try { if(Get-Command $command){RETURN $true }}
+Catch { RETURN $false } ; Finally { $ErrorActionPreference=$oldPreference }}
 
-function Load-Chimera {
-$TestChimera = Test-Command bash ; if($TestChimera -in 'True'){ 
-Write-Host "[+] Loading Chimera and doing some obfuscation.. " -ForegroundColor Blue -NoNewline
-bash ./Resources/Chimera/Chimera.sh -z -q -f $InvokePath -l 5 -v -t -c -i -p -h -s -b -j -k -e 2>&1> $null
-$Chimera = ls *chimera* 2> $null ; Clear-Content $InvokePath ; Add-Content $InvokePath $(Get-Content $Chimera)
-Write-Host "[OK]" -ForegroundColor Green ; Write-Host ; del $Chimera -ErrorAction SilentlyContinue }}
+function Load-Chameleon {
+$TestChameleon = Test-Command python3 ; if ($TestChameleon -in 'True'){ 
+Write-Host "[+] Loading Chameleon and doing some obfuscation.. " -ForegroundColor Blue -NoNewline
+$Chameleon = python3 ./Resources/Chameleon/chameleon.py -a $InvokePath -o $InvokePath 2>&1> $null
+Write-Host "[OK]" -ForegroundColor Green ; Write-Host ; del function_mapping.json -ErrorAction SilentlyContinue }}
 
 function Load-BetterXencrypt {
-Write-Host "[+] Loading BetterXencrypt and doing some encryption with $RandomNumber iterations.. " -ForegroundColor Blue -NoNewline
+if($Technique -in 'All') { Write-Host "[!] Avoid mixing BetterXencrypt with another techniques.. " -ForegroundColor Red -NoNewline }
+else { Write-Host "[+] Loading BetterXencrypt and doing some encryption with random iterations.. " -ForegroundColor Blue -NoNewline
 if ($local){ Import-Module $pwd\Resources\BetterXencrypt\BetterXencrypt.ps1 -Force } else {
 (New-object System.net.webclient).DownloadFile("https://raw.githubusercontent.com/JoelGMSec/Invoke-Stealth/main/Resources/BetterXencrypt/BetterXencrypt.ps1","$pwd/BetterXencrypt.ps1")
-Import-Module $pwd\BetterXencrypt.ps1 -Force } ; Invoke-BetterXencrypt -InFile $InvokePath -OutFile $InvokePath -Iterations $RandomNumber 2>&1> $null
+Import-Module $pwd\BetterXencrypt.ps1 -Force } ; Invoke-BetterXencrypt -InFile $InvokePath -OutFile $InvokePath -Iterations $RandomNumber 2>&1> $null }
 Write-Host "[OK]" -ForegroundColor Green ; Write-Host ; del BetterXencrypt.ps1 -ErrorAction SilentlyContinue }
 
 function Load-PyFuscation {
-$TestPyFuscation = Test-Command python3 ; if($TestPyFuscation -in 'True'){ 
-if($Technique -like '*Chimera*' -or '*all*') { $some = 'more' } else { $some = 'some' }
+$TestPyFuscation = Test-Command python3 ; if ($TestPyFuscation -in 'True'){ 
+if($Technique -like '*Chameleon*' -or '*all*') { $some = 'more' } else { $some = 'some' }
 Write-Host "[+] Loading PyFuscation and doing $some obfuscation.. " -ForegroundColor Blue -NoNewline
 $PyExecution = python3 ./Resources/PyFuscation/PyFuscation.py -fvp --ps $InvokePath 2> $null ; if($PyExecution) {
 $PyFuscation = "$pwd/Resources/PyFuscation/tmp/script.ps1" ; Clear-Content $InvokePath ; Add-Content $InvokePath $(Get-Content $PyFuscation -ErrorAction SilentlyContinue)}
@@ -86,17 +84,18 @@ Add-Content $InvokePath '$pwn = "iN"+"vo"+"Ke"+"-"+"eX"+"Press"+"ioN" ; New-Alia
 Write-Host "[OK]" -ForegroundColor Green ; Write-Host }
 
 function Load-PSObfuscation {
-if($Technique -like '*PSObfuscation*' -or '*all*') { Write-Host "[+] Loading PSObfuscation and randomizing script.. " -ForegroundColor Blue -NoNewline
+Write-Host "[+] Loading PSObfuscation and randomizing script.. " -ForegroundColor Blue -NoNewline
 if ($local){ Import-Module $pwd\Resources\PSObfuscation\Invoke-PSObfuscation.ps1 -Force } else {
 Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/JoelGMSec/Invoke-Stealth/master/Resources/PSObfuscation/Invoke-PSObfuscation.ps1')}
-$PSobfuscated = Invoke-PSObfuscation -Path $InvokePath -Comments -Variables -OutFile $InvokePath
-Write-Host "[OK]" -ForegroundColor Green ; Write-Host }}
+$PSobfuscation = Invoke-PSObfuscation -Path $InvokePath -Comments -Variables -OutFile $InvokePath
+Write-Host "[OK]" -ForegroundColor Green ; Write-Host }
 
-if($Technique -like '*Chimera*') { Load-Chimera }
+# Main
+if($Technique -like '*Chameleon*') { Load-Chameleon }
 if($Technique -like '*BetterXencrypt*') { Load-BetterXencrypt }
 if($Technique -like '*PyFuscation*') { Load-PyFuscation }
 if($Technique -like '*ReverseB64*') { Rev64-Encoder }
 if($Technique -like '*PSObfuscation*') { Load-PSObfuscation }
 
-if($Technique -in 'All') { Load-Chimera ; Load-BetterXencrypt ; Load-PyFuscation ; Rev64-Encoder ; Load-PSObfuscation }
+if($Technique -in 'All') { Load-Chameleon ; Load-BetterXencrypt ; Load-PyFuscation ; Rev64-Encoder ; Load-PSObfuscation }
 if($NoBanner -notlike '*-nobanner') { Write-Host "[+] Done! " -ForegroundColor Green ; Write-Host }
